b9bc817e3369203bcb43e72b86b790f0800f5bf1
Jose Cruz <resoj.cruz@gmail.com>
2024-02-19 12:02:57 -0500

    Refactor grid generation algorithm

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const gridSize = 600;
 
-let rows = 16;
-let cols = 16;
+let gridDimensions = 16;
 
 const container = document.querySelector('.container');
-container.style.width = `${gridSize}px`;
-container.style.height = `${gridSize}px`;
+container.style.width = container.style.height = `${gridSize}px`;
 
 function createGridCells() {
-    for (let i = 0; i < (rows * cols); i++) {
+    const numOfGridCells = (gridDimensions * gridDimensions);
+    for (let i = 0; i < numOfGridCells; i++) {
         const gridCell = document.createElement('div');
 
-        gridCell.style.width = `${(gridSize / cols) - 2}px`;
-        gridCell.style.height = `${(gridSize / rows) - 2}px`;
+        gridCell.style.width = gridCell.style.height = `${(gridSize / gridDimensions) - 2}px`;
         gridCell.classList.add('cell');
 
         container.appendChild(gridCell);
